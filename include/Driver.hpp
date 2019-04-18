3b544fde408a1da9fd1feeceffbe93d493f511cc
Zachary Kramer <kramer.zachary.nau@gmail.com>
2019-04-17 22:28:49 -0700

    Parallelize at k-mer level

--- include/Driver.hpp
+++ include/Driver.hpp
@@ -4,6 +4,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <algorithm>
+#include <string>
+#include <vector>
 #include "FASTA.hpp"
 #include "Levenshtein.hpp"
 #include "Rank.hpp"
@@ -20,33 +22,52 @@ inline void Run(const std::string& filePath,
                 const std::string& substring,
                 const bool verbose = (true)) {
     const Index substringLength = substring.length();
-    
+
     // Read in FASTA file
     const auto sequences = ReadFASTA(filePath);
 
     // Prepare to store rankings
-    std::vector<Rank> globalRanks(sequences.size() * 2);  // 2 scores per sequence
+    std::vector<Rank> globalRanks(sequences.size());
+
+    // If we are dealing with more than 4 sequences, parallelize
+    // at the sequence level. Otherwise parallelize at the k-mer level.
+    bool parallelizeKMers = (sequences.size() < 4) ? true : false;
 
     // Go through each sequence
     Index i;
-    #pragma omp parallel for schedule(dynamic) private(i)
+    #pragma omp parallel for schedule(dynamic) private(i) \
+    if (!parallelizeKMers)
     for (i = 0; i < sequences.size(); ++i) {
-        std::vector<Rank> localRanks;
+        // Calculate how many k-mers to calculate at a time
         // For now get all substrings at once
+        const Index numKMers = sequences.at(i).DNA.length() - 2*substringLength;
+
+        // Prepare to store ranks
+        std::vector<Rank> localRanks(numKMers);
+
         // TODO(Someone): Only generate a certain amount at a time
         const auto substrings = GetSubstrings(0,
                                               substringLength,
-                                              sequences.at(i).DNA.length() - 2*substringLength,
+                                              numKMers,
                                               sequences.at(i).DNA);
-        for (const auto& substr : substrings) {
+
+        // Now calculate the Levenshtein distance for each k-mer
+        Index j;
+        const std::string id = sequences.at(i).id;
+        // Create a fake rank
+        Rank bestRank;
+        bestRank.levenshtein = 100000000;
+        // Parallelize
+        #pragma omp parallel for schedule(dynamic) \
+        private(j) firstprivate(id) \
+        if (parallelizeKMers)
+        for (j = 0; j < substrings.size(); ++j) {
             // Calculate distance
-            localRanks.push_back(Rank(sequences.at(i).id, substr, LevenshteinDistance(substr, substring)));
+            const Rank rank(id, substrings.at(j), LevenshteinDistance(substrings.at(j), substring));
+            if (rank < bestRank) bestRank = rank;
         }
-        // Sort in ascending order
-        std::sort(localRanks.begin(), localRanks.end());
-        // Take the best two
-        globalRanks.at(2*i) = localRanks.at(0);
-        globalRanks.at(2*i+1) = localRanks.at(1);
+        // Save best substring + score
+        globalRanks.at(i) = bestRank;
     }
 
     // Output best scores
--- test/TestBenchmark.cpp
+++ test/TestBenchmark.cpp
@@ -7,12 +7,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 TEST(Benchmark, EColi) {
     // TODO(Claudia): Change to E. Coli
-    const std::string filePath = "../test/data/11-Small.fasta";
+    const std::string filePath = "../test/data/E-Coli.fna";
     const std::string substring = "ACTCACCACTGT";
 
     // omp_get_wtime() is very accuracte and measures in seconds
     double start = omp_get_wtime();
-    FuzzyMatch::Run(filePath, substring, false);
+    FuzzyMatch::Run(filePath, substring, true);
     double end = omp_get_wtime();
     std::cout << end - start << " seconds" << std::endl;
 }
